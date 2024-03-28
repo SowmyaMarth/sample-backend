550cdf092f0306aa472ea4ffc12170e0a9276a9e
JANYA <JANYA@DESKTOP-NG4E5FO>
2024-03-28 17:55:27 +0530

    Testing

---
+++ sample-backend/src/main/java/com/example/demo/SampleBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleBackendApplication.class, args);
+		System.out.println("test");
+	}
+
+}
